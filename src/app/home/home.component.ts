aab0bc4744ff7c49a6fb59d5b614a444f0a85ee4
thuannguyen <ttn173@ucsd.edu>
2018-07-22 00:38:04 -0700

    paginator, need to test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { RequestService } from '../services/request.service'
 import {Book} from '../classes/Book'
+import {PageEvent} from '@angular/material';
+import {MatPaginator} from '@angular/material/paginator';
+import {tap} from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
   books: [Book]
+  booksPerPage: Book[]
   loading: Boolean
+  pageEvent: PageEvent;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  
   constructor(private requestService :  RequestService) { }
 
   ngOnInit() {
@@ -18,11 +25,35 @@ export class HomeComponent implements OnInit {
         this.loading = false
       }, 4000)
       this.books = res
+      this.booksPerPage = this.books.slice(0, 4)
       var i = 0;
       for ( i = 0; i < this.books.length; i++){
         this.getAndSetImage(i)
       }
     })
+  }
+  getData(page: PageEvent){
+    console.log(event)
+    if (page.previousPageIndex == page.pageIndex -1){ //next
+      console.log("next")
+      var to = (page.pageIndex+1)*4
+      console.log("to b4 " + to)
+      if (to > this.books.length){
+        to = this.books.length
+        console.log("to af " + to)
+      }
+      this.booksPerPage = this.books.slice((page.previousPageIndex+1)*4, to )
+    }
+    else { //back
+      console.log(page.pageIndex)
+      console.log(page.previousPageIndex)
+      this.booksPerPage = this.books.slice(page.pageIndex*4, (page.previousPageIndex)*4)
+    }
+  }
+  loadNextPage(pageEvent: PageEvent){
+    console.log(pageEvent.length)
+    console.log(pageEvent.pageIndex)
+    this.booksPerPage = this.books.slice(1, 3);
   }
 
   getAndSetImage(index){
