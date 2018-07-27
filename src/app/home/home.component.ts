230adec827aceb2376894574ae2b3ec858fb9986
thuannguyen <ttn173@ucsd.edu>
2018-07-26 22:39:39 -0700

    search on keyup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,6 @@ import {MatSnackBar} from '@angular/material';
 })
 export class HomeComponent implements OnInit{
   books: Book[]
-  booksPerPage: Book[]
   loading: Boolean
   searchValue: String;
   numPerPage: number
@@ -30,10 +29,8 @@ export class HomeComponent implements OnInit{
     //binding the searchValue as obserable from util service, it will listen on value changes
     this.utilService.currentSerchValue.subscribe(searchValue => this.onSearch(searchValue));
     this.loading = true
-    this.numPerPage = 10
     this.requestService.getBooks().subscribe(res=>{
       this.books = res
-      this.booksPerPage = this.books.slice(0, this.numPerPage)
       this.formatDate();
       var i = 0;
       for ( i = 0; i < this.books.length; i++){
@@ -43,41 +40,23 @@ export class HomeComponent implements OnInit{
       this.requestService.setCachedBooks(this.books)
     })
   }
-  getData(page: PageEvent){
-    if (page.previousPageIndex == page.pageIndex -1){ //next
-      var to = (page.pageIndex+1)*this.numPerPage
-      if (to > this.books.length){
-        to = this.books.length
-      }
-      this.booksPerPage = this.books.slice((page.previousPageIndex+1)*this.numPerPage, to + 1)
-    }
-    else { //back
-      this.booksPerPage = this.books.slice(page.pageIndex*this.numPerPage, (page.previousPageIndex)*this.numPerPage)
-    }
-  }
 
   onSearch(searchValue){
     this.loading = true
+    this.books = this.requestService.getCachedBooks();
     if(!searchValue && this.books != null){
-      this.booksPerPage = this.books.slice(0, this.numPerPage)
       this.loading = false
-      return;
     }
-    if(searchValue) {
-      this.books = this.requestService.getCachedBooks();
-
+    else{
       if (searchValue && searchValue.trim() != '') {
-        //TODO possible issue when the result of filter is > this.numPerPage
-        this.booksPerPage = this.books.filter((item) => {
+        this.books= this.books.filter((item) => {
           this.loading = false
           return (item.title.toLowerCase().indexOf(searchValue.toLowerCase()) > -1);
-        }).slice(0,this.numPerPage)
-      }else {
-        this.loading = false
-        this.booksPerPage = this.books.slice(0, this.numPerPage)
+        })
       }
     }
   }
+
   formatDate(){
     this.books.forEach(book => {
         book.create_date = this.utilService.formatDate(book.create_date);
